2e23bcd2a560733ac2a31d2a22c34e660e6c41d6
Vanya Linov <lenev.2001@gmail.com>
2025-11-20 23:39:31 +0200

    Task1h2

---
+++ src/homework/Calculator.java
@@ -0,0 +1,13 @@
+// Створи в уже створеному проєкті новий клас Calculator.
+//Додай до нього метод add, який приймає два числа і повертає їхню суму (використай return).
+//Виклич цей метод у методі main і виведи результат.
+
+package homework;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+
+}
---
+++ src/homework/Task1h2.java
@@ -0,0 +1,24 @@
+// Створи в уже створеному проєкті новий клас Calculator.
+//Додай до нього метод add, який приймає два числа і повертає їхню суму (використай return).
+//Виклич цей метод у методі main і виведи результат.
+
+
+package homework;
+
+import java.util.Scanner;
+
+public class Task1h2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Введіть перше число: ");
+        int number1 = scan.nextInt();
+        System.out.print("Введіть друге число: ");
+        int number2 = scan.nextInt();
+
+        Calculator calc = new Calculator();
+        int result = calc.add(number1 , number2);
+
+        System.out.print("Cумма = " + result);
+    }
+}
