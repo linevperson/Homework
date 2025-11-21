fc8972d1b162da681c9e3e1545357aadf17f7f15
Roman <sityukroman44@gmail.com>
2025-11-21 17:57:59 +0200

    review exeptions

--- src/homework/Task4h2.java
+++ src/homework/Task4h2.java
@@ -18,7 +18,7 @@ public class Task4h2 {
         division(a , b);
     }
 
-    public static void division(int a , int b) {
+    public static void division(int a , int b) { //TODO метод працює, але щоб він був більш універсальний зроби щоб він повертав значення, а не тільки виводив результат(так його можна буде перевикористовувати для інших задач)
         try {
             int result = a / b;
             System.out.print("Результат: " + result);
