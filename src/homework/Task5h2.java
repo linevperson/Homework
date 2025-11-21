bc23360d068d90563cb700e4eb6e359e09b9d028
Vanya Linov <lenev.2001@gmail.com>
2025-11-21 16:51:50 +0200

    Task5h2

---
+++ src/homework/Task5h2.java
@@ -0,0 +1,20 @@
+//Створи перевірку очікуваного і фактичного значення:
+//Створи дві змінні: expected = "Login successful" і actual = "Login successful".
+//Додай умову, яка перевіряє, чи збігаються ці значення, і виводить "Тест пройдено" або "Тест не пройдено".
+
+package homework;
+
+public class Task5h2 {
+    public static void main(String[] args) {
+        String expected = "login successful" ;
+        String actual = "login successful" ;
+
+        if (expected.equals(actual)) {
+            System.out.print("Успішний тест");
+        } else {
+            System.out.print("Тест не пройдено");
+        }
+    }
+}
+
+//Хочу більш детальніше розібрати це завдання,бо трохи не можу зметчити логіку
