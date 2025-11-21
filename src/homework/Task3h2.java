eb52db3577cafb8975ab55a1edcb9155f8b6aad0
Roman <sityukroman44@gmail.com>
2025-11-21 17:38:52 +0200

    review ArrayList

--- src/homework/Task3h2.java
+++ src/homework/Task3h2.java
@@ -7,12 +7,12 @@ import java.util.ArrayList;
 
 public class Task3h2 {
     public static void main (String[] args) {
-        ArrayList<String> cities = new ArrayList<>();
+        ArrayList<String> cities = new ArrayList<>();//TODO список має бути одразу з 3ма містами - використай List.of і потім додай ще одне місто через add
         cities.add("Київ");
         cities.add("Харків");
         cities.add("Одеса");
         cities.add("Львів"); //Додав четверте місто
-        System.out.print(cities);
+        System.out.print(cities);//TODO тут виведи кожен елемент списку окремо (використовуй цикл)
 
     }
 
