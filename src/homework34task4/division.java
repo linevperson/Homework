eb388a7923f9b7d2253fc3bf5c1220a3c605dc3c
Vanya Linov <lenev.2001@gmail.com>
2025-11-22 22:21:05 +0200

    Task4h2

--- src/homework34task4/division.java
+++ src/homework34task4/division.java
@@ -1,11 +1,11 @@
 //Створи метод, який ділить два числа.
 // Використай try-catch, щоб обробити помилку ділення на нуль і вивести повідомлення "На нуль ділити не можна".
 
-package homework;
+package homework34task4;
 
 import java.util.Scanner;
 
-public class Task4h2 {
+public class division {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
@@ -18,12 +18,14 @@ public class Task4h2 {
         division(a , b);
     }
 
-    public static void division(int a , int b) { //TODO метод працює, але щоб він був більш універсальний зроби щоб він повертав значення, а не тільки виводив результат(так його можна буде перевикористовувати для інших задач)
+    public static Integer division(int a , int b) { //TODO метод працює, але щоб він був більш універсальний зроби щоб він повертав значення, а не тільки виводив результат(так його можна буде перевикористовувати для інших задач)
         try {
             int result = a / b;
             System.out.print("Результат: " + result);
+            return result;
         } catch (ArithmeticException error) {
             System.out.print("Не можна ділити на нуль");
+            return null;
         }
     }
 }
