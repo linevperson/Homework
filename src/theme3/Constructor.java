5b5c72d79fb884140fa9044346420f34fcda23e3
Vanya Linov <lenev.2001@gmail.com>
2025-12-17 02:14:56 +0200

    ex4

--- src/theme3/Constructor.java
+++ src/theme3/Constructor.java
@@ -5,9 +5,12 @@ public class Constructor {
 
         Product assortmentTesla = new Product("Tesla model 3" , 25000, true);
         Product assortmentBMW = new Product("BMW M3" , 53000, true);
+        DiscountedProduct discount15 = new DiscountedProduct("Tesla model 3" , 25000, true , 15);
 
         assortmentTesla.printProductInfo();
         assortmentBMW.printProductInfo();
+
+        System.out.println( "Ціна Tesla model 3 зі знижкою: " + discount15.getDiscount() );
 
     }
 }
@@ -40,7 +43,18 @@ class Product {
         System.out.println("Ціна: " + price);
         System.out.println("Наявність: " + availability);
     }
-    
 
 }
 
+class DiscountedProduct extends Product {
+    public double discount;
+
+    public DiscountedProduct (String name , double price , boolean availability , double discount) {
+        super(name , price , availability);
+        this.discount = discount ;
+    }
+    public double getDiscount () {
+        return getPrice() - (getPrice() * discount / 100);
+    }
+
+}
