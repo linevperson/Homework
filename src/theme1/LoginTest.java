b631b4feb1c49f6e60b0ab46318d83f925aaa679
Vanya Linov <lenev.2001@gmail.com>
2026-01-06 17:39:01 +0200

    WebChrome test title

--- src/theme1/LoginTest.java
+++ src/theme1/LoginTest.java
@@ -15,7 +15,7 @@ public class LoginTest {
         String title = driver.getTitle();
 
         assert title != null; //тут мені видало лампочку,що може бути помилка на contains.Вирішив довіритись і додав цю строчку.Не треба було?)
-        if (title.contains("Swag Labs")) {
+        if (title.contains("Swag Labs")) { //або можна було написати title.equals для 100% співпадіння.Правильно розумію?
             System.out.println("Тест успішний!");
         } else {
             System.out.println("Тест не пройдено!");
