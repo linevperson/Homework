10fcec302a06f97c8b7110b081aeb6742b31f417
Vanya Linov <lenev.2001@gmail.com>
2026-01-06 16:22:58 +0200

    test title

---
+++ src/theme1/LoginTest.java
@@ -0,0 +1,26 @@
+//Додай перевірку, що заголовок сторінки містить слово "Swag Labs".
+// Виведи результат у консоль.
+
+package theme1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+    public static void main (String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.saucedemo.com/");
+
+        String title = driver.getTitle(); //а як він розуміє
+
+        assert title != null; //тут мені видало лампочку,що може бути помилка на contains.Вирішив довіритись і додав цю строчку.Не треба було?)
+        if (title.contains("Swag Labs")) {
+            System.out.println("Тест успішний!");
+        } else {
+            System.out.println("Тест не пройдено!");
+        }
+
+        driver.quit();
+    }
+}
