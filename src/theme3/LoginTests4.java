ecb41414f80e9ffb67e986b7d0fee80b33aa024c
Vanya Linov <lenev.2001@gmail.com>
2026-01-18 14:50:59 +0200

    theme3ex5

---
+++ src/theme3/LoginTests4.java
@@ -0,0 +1,47 @@
+//Реалізуй DataProvider, щоб передати кілька пар логін/пароль:
+//Створи тест, який для кожної пари вводить ці дані і виводить їх у консоль (поки без реальної авторизації).
+
+package theme3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class LoginTests4 {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @DataProvider (name = "loginData")
+    public Object [][] loginData () {
+        return new Object[][] {
+                {"standard_user", "secret_sauce"},
+                {"locked_out_user", "secret_sauce"},
+                {"problem_user", "secret_sauce"},
+                {"error_user", "secret_sauce"},
+        };
+    }
+
+    @Test (dataProvider = "loginData")
+    public void LoginTest(String username , String password) {
+        driver.findElement(By.id("user-name")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+
+    }
+
+    @AfterMethod
+    public void closeTest() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
